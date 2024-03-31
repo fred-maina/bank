c414829624e5fb84a6678d5244408b624eebb7c7
fred-maina <fchege04@gmail.com>
2024-03-31 22:57:09 +0300

    Add Comments and Documentation for functions

--- accounts.py
+++ accounts.py
@@ -1,58 +1,79 @@
-from bank_account import Bank  
+from bank_account import Bank  # Importing Bank class from bank_account module
 import random
-bank=Bank("KCB BANK")
+
+bank = Bank("Py Bank")  # Creating an instance of Bank class with the bank name "Py Bank"
+
 def account_operations(account_number):
-    print("Welcome to your account choose any of the following Services: ")
-    while(True):
+    """
+    Function to perform operations on a bank account.
+
+    Parameters:
+        account_number (int): The account number of the bank account.
+
+    Returns:
+        None
+    """
+    print("Welcome to your account. Choose any of the following services: ")
+    while True:
         try:
-            j=int(input("choose 1 for account balance and Information. Choose 2 to deposit choose 3 to withdraw and 4 to exit: "))
+            j = int(input("Choose 1 for account balance and information. Choose 2 to deposit. Choose 3 to withdraw. Choose 4 to exit: "))
             if j == 1:
                 bank.displayInformation(account_number)
             elif j == 2:
-                amount=int(input("Enter the amount of money you would like to Deposit: "))
-                pin=int(input("Input Your PIN: "))
-                bank.perform_transaction(account_number,pin,"Deposit",amount)
-            elif j ==3 :
-                amount=int(input("Please Input the amount You would Like to Withdraw: "))
-                pin=int(input("Input Your PIN: "))
-                bank.perform_transaction(account_number,pin,"Withdraw",amount)
-            elif j == 4 :
-                print("Thank you for banking with us. Have a Great day")
+                amount = int(input("Enter the amount of money you would like to deposit: "))
+                pin = int(input("Input your PIN: "))
+                bank.perform_transaction(account_number, pin, "Deposit", amount)
+            elif j == 3:
+                amount = int(input("Please input the amount you would like to withdraw: "))
+                pin = int(input("Input your PIN: "))
+                bank.perform_transaction(account_number, pin, "Withdraw", amount)
+            elif j == 4:
+                print("Thank you for banking with us. Have a great day!")
                 break
             else:
-                print("Invalid Selection Please Try Again")
+                print("Invalid selection. Please try again.")
         except ValueError as v:
-            print("Invalid selection Please Try again")
+            print("Invalid selection. Please try again.")
+
 def account_creation():
+    """
+    Function to create a new bank account.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     try:
         global accountNumber
-        accountNumber = random.randint(1000000, 9999999)
-        name = input("Enter your Name: ")
+        accountNumber = random.randint(1000000, 9999999)  # Generate a random account number
+        name = input("Enter your name: ")
         
         # Loop until a valid PIN is entered
         while True:
-            pin = input("Choose a 4 digit pin: ")
+            pin = input("Choose a 4 digit PIN: ")
             if len(pin) != 4 or not pin.isdigit():
-                print("Invalid PIN. Please input a 4 digit numeric pin!")
+                print("Invalid PIN. Please input a 4 digit numeric PIN!")
             else:
                 pin = int(pin)
                 break
-        bank.createAccount(accountNumber, name, pin)
-        account_operations(accountNumber)
+        bank.createAccount(accountNumber, name, pin)  # Create a new account
+        account_operations(accountNumber)  # Perform account operations for the newly created account
     except ValueError as v:
         print(v)
 
-if __name__ == "__main__" :
-    print("Hello There Welcome to Py-Bank Self-service Menu: ")
+if __name__ == "__main__":
+    print("Hello there! Welcome to Py-Bank Self-service Menu: ")
     try:
-        i=int(input("Input 1 if you would like to Create a New Acount or 2 To go to your Account: "))
+        i = int(input("Input 1 if you would like to create a new account or 2 to go to your account: "))
         if i == 1:
-            account_creation()
+            account_creation()  # Create a new account
         elif i == 2:
             try:
-                account_operations(accountNumber)
+                account_operations(accountNumber)  # Perform account operations for an existing account
             except NameError as n:
-                print("No accounts have been Added Please Create a new Account")
-                account_creation()
+                print("No accounts have been added. Please create a new account.")
+                account_creation()  # If no accounts exist, create a new account
     except ValueError as v:
         print(v)
